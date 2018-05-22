0cb4208bd03baef79bc3221f319e3952a39d55b7
ekalips <djqrj96@gmail.com>
2018-05-22 18:43:03 +0300

    Fixes corresponding to api

--- app/src/main/java/com/ekalips/cahscrowd/network/body/CreateActionBody.kt
+++ app/src/main/java/com/ekalips/cahscrowd/network/body/CreateActionBody.kt
@@ -2,4 +2,5 @@ package com.ekalips.cahscrowd.network.body
 
 import com.squareup.moshi.Json
 
-class CreateActionBody(@Json(name = "value") private val amount: Double)
+class CreateActionBody(@Json(name = "amount") private val amount: Double,
+                       @Json(name = "type") private val type: Int = 2)
